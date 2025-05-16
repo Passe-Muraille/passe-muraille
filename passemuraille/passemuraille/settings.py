58f418b6f7ebed993d32b7a45facf9f5a466ff37
Passe-Muraille <passemuraille2264@gmail.com>
2025-05-16 18:40:12 +0200

    Update settings.py

--- passemuraille/passemuraille/settings.py
+++ passemuraille/passemuraille/settings.py
@@ -132,7 +132,7 @@ DATABASES = {
         "USER": "postgres",
         "PASSWORD": "caeE1aGG2B5gg3DFf3g2e613CdF1352a",
         "HOST": "viaduct.proxy.rlwy.net",
-        "PORT": 5432,
+        "PORT": 41784,
     }
 }
 
